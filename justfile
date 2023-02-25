f710691b56ef706efba96c9bea339012c9c7e25f
Ryan P. Brewster <RyanPBrewster@gmail.com>
2023-02-25 12:30:13 -0500

    damn, random numbers worked

--- justfile
+++ justfile
@@ -4,3 +4,7 @@ inspect:
 echo:
 	cd echo && go build main.go
 	cd maelstrom && ./maelstrom test -w echo --bin ../echo/main --node-count 1 --time-limit 10
+
+unique-ids:
+	cd unique-ids && go build main.go
+	cd maelstrom && ./maelstrom test -w unique-ids --bin ../unique-ids/main --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
---
+++ unique-ids/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = rand.Int()
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
