acc1b741d8f3c429d8a0f8f60f18fed508785ce3
Ryan P. Brewster <RyanPBrewster@gmail.com>
2023-02-25 15:41:51 -0500

    implement multi-broadcast

--- broadcast/main.go
+++ broadcast/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -11,31 +13,64 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 
-	var seen []int
+	fout, err := os.CreateTemp("", "maelstrom-brodcast")
+	if err != nil {
+		log.Fatal(err)
+	}
+	l := log.New(fout, fmt.Sprintf("[%v]", n.ID()), log.LstdFlags)
+
+	seen := make(map[int]struct{})
+	var neighbors []string
 	var mu sync.Mutex
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		l.Printf("recv broadcast: %s", msg.Body)
+
 		var body broadcastRequest
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 		mu.Lock()
 		defer mu.Unlock()
-		seen = append(seen, body.Message)
+		if _, ok := seen[body.Message]; !ok {
+			seen[body.Message] = struct{}{}
+			l.Printf("%d is new", body.Message)
+			for _, neighbor := range neighbors {
+				l.Printf("mirroring to %v: %+v", neighbor, body)
+				n.Send(neighbor, body)
+			}
+		} else {
+			l.Printf("already seen %d", body.Message)
+		}
 		return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
 	})
 
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		// Our partner nodes will ack our broadcasts, and right now we're just ignoring that.
+		return nil
+	})
+
 	n.Handle("read", func(msg maelstrom.Message) error {
+		l.Printf("recv read: %s", msg.Body)
 		mu.Lock()
 		defer mu.Unlock()
-		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": seen})
+		messages := make([]int, 0, len(seen))
+		for m := range seen {
+			messages = append(messages, m)
+		}
+		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": messages})
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
+		l.Printf("recv topology: %s", msg.Body)
 		var body topologyRequest
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		mu.Lock()
+		defer mu.Unlock()
+		neighbors = body.Topology[n.ID()]
+		l.Printf("set neighbors=%v", neighbors)
 
 		return n.Reply(msg, map[string]any{"type": "topology_ok"})
 	})
@@ -46,9 +81,10 @@ func main() {
 }
 
 type broadcastRequest struct {
-	Message int
+	Type    string `json:"type"`
+	Message int    `json:"message"`
 }
 
 type topologyRequest struct {
-	Topology map[string][]string
+	Topology map[string][]string `json:"topology"`
 }
